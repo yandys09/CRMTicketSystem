297451351b0b8f422bf498f4fab75b561d0cd4c0
yandys09 <yandys03@naver.com>
2021-09-17 16:36:12 +0900

    App set up. added loging and reset password form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Entry } from "./pages/entry/Entry.page";
+
+function App() {
+  return (
+    <div className="App">
+      <Entry />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/entry/Entry.page.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Jumbotron } from "react-bootstrap";
+import { LoginForm } from "../../components/login/Login.comp";
+import { ResetPassword } from "../../components/password-reset/PasswordReset.comp";
+import "./entry.style.css";
+
+export const Entry = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [frmLoad, setFrmLoad] = useState("login");
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      return alert("Fill up all the form!");
+    }
+    console.log(email, password);
+  };
+
+  const handleOnResetSubmit = (e) => {
+    e.preventDefault();
+    if (!email) {
+      return alert("Please enter the email!");
+    }
+    console.log(email);
+  };
+
+  const formSwitcher = (frmType) => {
+    setFrmLoad(frmType);
+  };
+
+  return (
+    <div className="entry-page bg-info">
+      <Jumbotron className="form-box">
+        {frmLoad === "login" && (
+          <LoginForm
+            handleOnchange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+            formSwitcher={formSwitcher}
+            email={email}
+            pass={password}
+          />
+        )}
+
+        {frmLoad === "rest" && (
+          <ResetPassword
+            handleOnchange={handleOnChange}
+            handleOnResetSubmit={handleOnResetSubmit}
+            formSwitcher={formSwitcher}
+            email={email}
+          />
+        )}
+      </Jumbotron>
+    </div>
+  );
+};
