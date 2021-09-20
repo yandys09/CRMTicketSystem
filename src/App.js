3c25f3e6f2eb9db25551a0ef8cc38061f7623996
yandys09 <yandys03@naver.com>
2021-09-20 12:22:58 +0900

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import DefaultLayout from "./layout/DefaultLayout";
 import { Entry } from "./pages/entry/Entry.page";
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+      <DefaultLayout>//Dashboard</DefaultLayout>
     </div>
   );
 }
